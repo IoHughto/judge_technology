1bd68f42b2708ca0fcf1d9f5c8138340c295cd94
Joe Hughto <joe@qnect.com>
2018-01-18 15:11:38 -0500

    Updating about page

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'judge.technology' });
+    res.render('index', { title: 'judge.technology' });
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'judge.technology' });
 });
 
 module.exports = router;

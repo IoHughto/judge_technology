64f6c3bdc8d2a474243ddadf9207317a31d300ee
JoeHughto <joe.hughto@gmail.com>
2018-01-18 18:45:38 -0500

    Puts actual users in.  Still need to add the rest of the L3s.

--- web/db/users.js
+++ web/db/users.js
@@ -1,6 +1,26 @@
 var records = [
-    { id: 1, username: 'jack', password: 'secret', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
-    , { id: 2, username: 'jill', password: 'birthday', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
+    { id: 1, username: 'jkellman', password: '89556850', displayName: 'Jonah Kellman' },
+    { id: 2, username: 'tgulley', password: '3103902087', displayName: 'Trevor Gulley' },
+    { id: 3, username: 'mlinscott', password: '3223951534', displayName: 'Megan Linscott' },
+    { id: 4, username: 'jemery', password: '9101698104', displayName: 'Jeffrey Emery' },
+    { id: 5, username: 'ccahill', password: '98991010', displayName: 'Chris Cahill' },
+    { id: 6, username: 'ehare', password: '7208230135', displayName: 'Elizabeth Hare' },
+    { id: 7, username: 'eraff', password: '1208312089', displayName: 'Elliot Raff' },
+    { id: 8, username: 'cwendelboe', password: '1101745766', displayName: 'Chris Wendelboe' },
+    { id: 9, username: 'cfeatherer', password: '7102046136', displayName: 'Charles Featherer' },
+    { id: 10, username: 'tprodigalidad', password: '3214072503', displayName: 'Tate Prodigalidad' },
+    { id: 11, username: 'jsteet', password: '8102096198', displayName: 'Joe Steet' },
+    { id: 12, username: 'djackson', password: '5210119497', displayName: 'Daniel Jackson' },
+    { id: 13, username: 'aeidelsafy', password: '7208162501', displayName: 'Adam Eidelsafy' },
+    { id: 14, username: 'cculpon', password: '3103625099', displayName: 'Chase Culpon' },
+    { id: 15, username: 'crumore', password: '1201522643', displayName: 'Chris Rumore' },
+    { id: 16, username: 'hwiest', password: '2102314312', displayName: 'Hank Wiest' },
+    { id: 17, username: 'jkriner', password: '1203031923', displayName: 'Jacob Kriner' },
+    { id: 18, username: 'jeveland', password: '7223771915', displayName: 'James Eveland' },
+    { id: 19, username: 'ahullings', password: '2208467096', displayName: 'Anthony Hullings' },
+    { id: 20, username: 'gmedeiros', password: '8223744279', displayName: 'Gil Medeiros' },
+    { id: 21, username: 'rsalamin', password: '19566502', displayName: 'Risk Salamin' },
+    { id: 22, username: 'jhughto', password: '9102402960', displayName: 'Joe Hughto' }
 ];
 
 exports.findById = function(id, cb) {

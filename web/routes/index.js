5433440feec2a20b2cb982d522e8e2e00668728c
JoeHughto <joe.hughto@gmail.com>
2018-01-18 18:25:47 -0500

    Implements login portal for event feedback forms

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,13 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'judge.technology' });
+    res.render('index', { title: 'judge.technology', user: req.user });
 });
 
 router.get('/about', function(req, res, next) {
     res.render('about', { title: 'judge.technology' });
 });
 
-module.exports = router;
+router.get('/login',
+function(req, res){
+    res.render('login');
+});
+
+router.post('/login',
+passport.authenticate('local', { failureRedirect: '/login' }),
+function(req, res) {
+    res.redirect('/feedback');
+});
+
+router.get('/feedback',
+require('connect-ensure-login').ensureLoggedIn(),
+function(req, res){
+    res.render('feedback', { user: req.user });
+});
+
+module.exports = {
+    router: router,
+    passport: passport,
+    Strategy: Strategy};
